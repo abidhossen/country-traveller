8f011559d61d6f27f9469c7eb5fc0eb2b3a44a42
abidhossain15 <abidhossain3000@gmail.com>
2021-03-20 11:56:40 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Destination from './Components/Destination/Destination';
+import Contact from './Components/Contact/Contact';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/destination">
+          <Destination></Destination>
+        </Route>
+        <Route path="/contact">
+          <Contact></Contact>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
+
+
   );
 }
 
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Contact = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <h1>This Contact</h1>
         </div>
     );
 };
 
-export default Home;
+export default Contact;
